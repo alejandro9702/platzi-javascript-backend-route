cad4b25ffbc5ea5be748d003d766c05bbdd56d63
ArestrepoBlankfactor <alejandro.restrepo@blankfactor.com>
2022-10-18 17:16:02 -0500

    promises chapter

---
+++ asynchronism/src/promise/index.js
@@ -0,0 +1,42 @@
+const promise = new Promise((resolve, reject) => {
+  resolve("hey");
+});
+
+const cows = 9;
+
+const counCows = new Promise((resolve, reject) => {
+  if (cows > 10) {
+    resolve(`We have ${cows} cows on the farm`);
+  } else reject("There is not cows enough on the farm");
+});
+
+counCows
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally((err) => {
+    console.log("finally finished");
+  });
+
+function delay(time, message) {
+  return new Promise((resolve, reject) => {
+    if (time > 0) {
+      setTimeout(() => {
+        resolve(message);
+      }, time);
+    } else {
+      reject("time must be greater than zero");
+    }
+  });
+}
+
+delay(0, "Hello after 2s")
+  .then((message) => console.log(message))
+  .catch((err) => {
+    console.log(err);
+  });
+
+  
